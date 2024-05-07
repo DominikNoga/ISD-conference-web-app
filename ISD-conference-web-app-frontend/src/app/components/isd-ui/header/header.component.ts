f6264585758d6da70457f556d9f4dc9bbf7fb658
Dawid Jastrzębski <d.jastrzebski21@gmail.com>
2024-05-07 21:33:55 +0200

    kinda works

--- ISD-conference-web-app-frontend/src/app/components/isd-ui/header/header.component.ts
+++ ISD-conference-web-app-frontend/src/app/components/isd-ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { NavService } from 'src/app/services/nav/nav.service';
 
 @Component({
@@ -15,4 +15,28 @@ export class HeaderComponent implements OnInit {
   displayBurgerNav(): boolean {
     return this.navService.displayBurgerNav();
   }
+
+  isMenuVisible = true;
+  lastScrollTop = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    let currentScroll = window.scrollY ;
+    let delta = 5;
+    
+
+  if(Math.abs(this.lastScrollTop - currentScroll) >= delta){
+    if (currentScroll >= this.lastScrollTop && window.scrollY > 70) {
+      // Scrolling down
+      this.isMenuVisible = false;
+    } else {
+      // Scrolling up
+      this.isMenuVisible = true;
+    }
+    console.log("Current scroll:", currentScroll)
+    console.log("LastScrollTop: ", this.lastScrollTop)
+  
+    this.lastScrollTop = currentScroll;
+  }
+}
 }
